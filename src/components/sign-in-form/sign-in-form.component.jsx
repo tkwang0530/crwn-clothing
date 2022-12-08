f6b28b8578b37429429d33e01fd1853d42fac7b7
Tommy Wang <tommywang@17.media>
2022-12-08 00:50:12 -0800

    migrate to Styled-Components

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -2,11 +2,11 @@
 import { useState } from "react";
 
 import FormInput from "../form-input/form-input.component";
-import Button from "../button/button.component";
+import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
 
-import { signInWithGooglePopup, createUserDocumentFromAuth, signInAuthUserWIthEmailAndPassword } from "../../utils/firebase/firebase.utils";
+import { signInWithGooglePopup, signInAuthUserWIthEmailAndPassword } from "../../utils/firebase/firebase.utils";
 
-import './sign-in-form.styles.scss';
+import { SignInContainer, ButtonsContainer } from './sign-in-form.styles';
 
 const defaultFormFields = {
     email: '',
@@ -29,7 +29,7 @@ const SignInForm = () => {
         event.preventDefault();
 
         try {
-            const { user } = await signInAuthUserWIthEmailAndPassword(email, password);
+            await signInAuthUserWIthEmailAndPassword(email, password);
             resetFormFields();
         } catch (error) {
             switch(error.code) {
@@ -50,7 +50,7 @@ const SignInForm = () => {
         setFormFields({...formFields, [name]: value});
     }
     return (
-        <div className="sign-up-container">
+        <SignInContainer>
             <h2>Already have an account?</h2>
             <span>Sign in with your email and password</span>
             <form onSubmit={handleSubmit}>
@@ -58,13 +58,13 @@ const SignInForm = () => {
 
                 <FormInput label="Password" type="password" required onChange={handleChange} name="password" value={password}/>
 
-                <div className="buttons-container">
+                <ButtonsContainer>
                     <Button type="submit">Sign In</Button> 
-                    <Button type="button" buttonType='google' onClick={signInWithGoogle}>Google sign in</Button>
-                </div>
+                    <Button type="button" buttonType={BUTTON_TYPE_CLASSES.google} onClick={signInWithGoogle}>Google sign in</Button>
+                </ButtonsContainer>
                 
             </form>
-        </div>
+        </SignInContainer>
     )
 }
 
--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -20,7 +20,6 @@ export const UserProvider = ({ children }) => {
             if(user) {
                 createUserDocumentFromAuth(user);
             }
-            console.log(user);
             setCurrentUser(user);
         })
         return unsubscribe
